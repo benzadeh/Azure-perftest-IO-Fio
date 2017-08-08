8bca9d4219ead359092a7c358b36007faf6b415c
Ben Zadeh <benzadeh@users.noreply.github.com>
2017-08-08 13:53:04 -0400

    Update prep_disk.sh

--- prep_disk.sh
+++ prep_disk.sh
@@ -6,3 +6,13 @@ sudo mkfs.ext4 -m 0 -F -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/sd
 sudo mkdir -p /mnt/disks/200gb-dir
 #sudo mount -o discard,defaults /dev/[DEVICE_ID] /mnt/disks/[MNT_DIR]
 sudo mount -o discard,defaults /dev/sdb /mnt/disks/200gb-dir
+
+# Mounting 300GB
+
+sudo mkdir -p /mnt/disks/300gb-dir
+sudo mkfs.ext4 -m 0 -F -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/sde
+sudo mount -o discard,defaults /dev/sde /mnt/disks/300gb-dir
+
+
+
+
